d63f4f6f1d9420b69d42317c4dbf069bde0e107f
Adnan Mahmić <mahmica@gmail.com>
2022-07-23 13:13:11 +0200

    navbar and header added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Navbar from "./Navbar/Navbar";
+import './Header.scss'
+import Needs from "../Needs/Needs";
+
+
+const Header = () => {
+    return (
+        <section className="header">
+            <Container >
+                <Navbar></Navbar>
+                <h1 className="brand">EXCEPTIONAL</h1>
+                <h2 className="dev">DEVELOPMENT</h2>
+                <p className="paragraf">We offer the creation of websites, web applications and mobile applications <br></br>
+                with the help of the most popular technologies and the best software solutions</p>
+            </Container>
+        </section>
+    )
+}
+
+export default Header;
