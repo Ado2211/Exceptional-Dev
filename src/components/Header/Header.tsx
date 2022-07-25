9f5c7d2512a17029f93ec04324ad24b4bb794672
Adnan Mahmić <mahmica@gmail.com>
2022-07-25 13:53:41 +0200

    changes header, navbar styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,16 +5,19 @@ import './Header.scss'
 import Needs from "../Needs/Needs";
 
 
+
+
+
 const Header = () => {
     return (
-        <section className="header">
-            <Container >
+        <section className="header" >
+           
                 <Navbar></Navbar>
-                <h1 className="brand">EXCEPTIONAL</h1>
+                <h1 className="brand ">EXCEPTIONAL</h1>
                 <h2 className="dev">DEVELOPMENT</h2>
                 <p className="paragraf">We offer the creation of websites, web applications and mobile applications <br></br>
                 with the help of the most popular technologies and the best software solutions</p>
-            </Container>
+            
         </section>
     )
 }
