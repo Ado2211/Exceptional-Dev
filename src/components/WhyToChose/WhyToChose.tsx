dfbec6730ab6a7c35441d3c4b5b891b9ac2c2cf0
Adnan Mahmić <mahmica@gmail.com>
2022-07-27 04:04:24 +0200

    implement section  "whyToChose"

--- src/components/WhyToChose/WhyToChose.tsx
+++ src/components/WhyToChose/WhyToChose.tsx
@@ -3,38 +3,37 @@ import { Container } from "react-bootstrap";
 import './WhyToChose.scss'
 import images from "../../assets/images";
 
-
 const WhyToChose = () => {
     return (
-        <section className="whyToChose">
-            <Container>
-                <h1 className="naslovWhy">Why to Chose Exceptional Development?</h1>
-                <div className="squares">
-                    <div className="square">
-                        <img className="iconsWhy" src={images.person} alt="" />
-                        <h3>User Friendly</h3>
-                        <p>Neki tekst</p>
-                    </div>
-
-                    <div className="square">
-                        <img src="" alt="" />
-                        <h3>User Friendly</h3>
-                        <p>Neki tekst</p>
-                    </div>
-
-                    <div className="square">
-                        <img src="" alt="" />
-                        <h3>User Friendly</h3>
-                        <p>Neki tekst</p>
-                    </div>
-
-                    <div className="square">
-                        <img src="" alt="" />
-                        <h3>User Friendly</h3>
-                        <p>Neki tekst</p>
-                    </div>
+        <section className="whyToChose ">
+            <img className="imgBg " src={images.bgWhy} alt="" />
+            <h1 className="naslovWhy">Why to Chose Exceptional Development?</h1>
+            <div className="squares">
+                <div className="square">
+                    <img className="iconsWhy" src={images.person} alt="" />
+                    <h3>User Friendly</h3>
+                    <p>Neki tekst</p>
                 </div>
-            </Container>
+
+                <div className="square active">
+                    <img className="iconsWhy" src={images.handShake} alt="" />
+                    <h3>Trust</h3>
+                    <p>Neki tekst</p>
+                </div>
+
+                <div className="square">
+                    <img className="iconsWhy" src={images.shield} alt="" />
+                    <h3>Guarantee</h3>
+                    <p>Neki tekst</p>
+                </div>
+
+                <div className="square">
+                    <img className="iconsWhy" src={images.time} alt="" />
+                    <h3>User Friendly</h3>
+                    <p>Neki tekst</p>
+                </div>
+            </div>
+
         </section>
     )
 }
