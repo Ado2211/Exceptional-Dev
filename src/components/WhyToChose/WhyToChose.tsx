de8d0508de9779178b8705362ee647b9c1ed2303
Adnan Mahmić <mahmica@gmail.com>
2022-07-28 16:41:27 +0200

    updates

--- src/components/WhyToChose/WhyToChose.tsx
+++ src/components/WhyToChose/WhyToChose.tsx
@@ -4,37 +4,64 @@ import './WhyToChose.scss'
 import images from "../../assets/images";
 
 const WhyToChose = () => {
+    function handleHover() {
+            let square = document.getElementById('userFriendly');
+
+    }
+
     return (
         <section className="whyToChose ">
             <img className="imgBg " src={images.bgWhy} alt="" />
             <h1 className="naslovWhy">Why to Chose Exceptional Development?</h1>
             <div className="squares">
                 <div className="square">
-                    <img className="iconsWhy" src={images.person} alt="" />
+                    <img onMouseOver={handleHover} className="iconsWhy" id="userFriendly" src={images.person} alt="" />
                     <h3>User Friendly</h3>
-                    <p>Neki tekst</p>
+                    <p>Neki tekst ovdje treba</p>
+                    <p>Neki tekst ovdje </p>
                 </div>
 
                 <div className="square actives">
-                    <img className="iconsWhy" src={images.handShake} alt="" />
+                    <img className="iconsWhy" id="trust" src={images.handShake} alt="" />
                     <h3>Trust</h3>
-                    <p>Neki tekst</p>
+                    <p>Neki tekst ovdje treba</p>
+                    <p>Neki tekst ovdje </p>
                 </div>
 
                 <div className="square">
-                    <img className="iconsWhy" src={images.shield} alt="" />
+                    <img className="iconsWhy" id="shield" src={images.shield} alt="" />
                     <h3>Guarantee</h3>
-                    <p>Neki tekst</p>
+                    <p>Neki tekst ovdje treba</p>
+                    <p>Neki tekst ovdje </p>
                 </div>
 
                 <div className="square">
-                    <img className="iconsWhy" src={images.time} alt="" />
-                    <h3>User Friendly</h3>
-                    <p>Neki tekst</p>
+                    <img className="iconsWhy" id="onTime" src={images.time} alt="" />
+                    <h3>On Time</h3>
+                    <p>Neki tekst ovdje treba</p>
+                    <p>Neki tekst ovdje </p>
                 </div>
             </div>
 
+            <div className="aboutUser about"><b>Exceptional development</b> is a team of young but
+                experienced and responsible people in their work.<br></br>
+                With a lot of experience and responsibility, <b>we are ready for the challenges</b> <br></br>
+                you have for us.
+            </div>
+
+            <div className="abouTrust about active">With a lot of experience and responsibility,
+                <b>we are ready for the challenges</b> <br></br>
+            </div>
+
+            <div className="abouGuarantee about">With a lot of experience
+            </div>
+
+            <div className="aboutOnTime about">experience and responsibility,
+            </div>
+
         </section>
+
+        
     )
 }
 
