e8fdb5a9d1203897132680d7256c7119597c16c8
Adnan Mahmić <mahmica@gmail.com>
2022-08-01 16:45:34 +0200

    what we do updates

---
+++ src/components/WhatWeDo/WhatWeDo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './WhatWeDo.scss'
+
+const WhatWeDo = () => {
+    return (
+        <section className="whatWeDo">
+            <h1 className="naslovWhy">What We Do ?</h1>
+        </section>
+    )
+}
+
+export default WhatWeDo;
