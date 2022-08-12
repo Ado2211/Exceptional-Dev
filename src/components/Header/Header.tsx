56b0c3fb3c9a7ed962c7a687a50865681f452e46
Adnan Mahmić <mahmica@gmail.com>
2022-08-12 17:52:42 +0200

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,17 +10,19 @@ import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
     return (
-        
+
         <section className="section header" >
             <Container>
-             <Navbar></Navbar>
-         
-           
-            <h1 className="brand ">EXCEPTIONAL</h1>
-            <h2 className="dev">DEVELOPMENT</h2>
-            <p className="paragraf">We offer the creation of websites, web applications and mobile applications <br></br>
-                with the help of the most popular technologies and the best software solutions</p>
-                </Container>
+                <Navbar></Navbar>
+                <div className="naslov">
+                    <div >
+                        <h1 className="brand ">EXCEPTIONAL</h1>
+                        <h2 className="dev">DEVELOPMENT</h2>
+                        <p className="paragraf">We offer the creation of websites, web applications and mobile applications <br></br>
+                            with the help of the most popular technologies and the best software solutions</p>
+                    </div>
+                </div>
+            </Container>
         </section>
     )
 }
